23e6e72ef8a2ddb97669a9f6e16252db9e298157
Damian Escobedo <damian050697@gmail.com>
2026-06-08 18:11:12 -0600

    enforce screen boundaries

--- code/game.c
+++ code/game.c
@@ -21,38 +21,57 @@ void drawGrid(WindowBuffer *windowBuffer) {
     }
 }
 
-void updatePlayerPosition(GameState *gameState, float deltaTimeMs) {
+void updatePlayerPosition(GameState *gameState, int width, int height, float deltaTimeMs) {
+    float newX = gameState->player.position.x;
+    float newY = gameState->player.position.y;
+
     switch (gameState->player.direction) {
         case UP: {
-            gameState->player.position.y -= (SPEED * deltaTimeMs);
+            newY -= (SPEED * deltaTimeMs);
         } break;
         case RIGHT: {
-            gameState->player.position.x += (SPEED * deltaTimeMs);
+            newX += (SPEED * deltaTimeMs);
         } break;
         case DOWN: {
-            gameState->player.position.y += (SPEED * deltaTimeMs);
+            newY += (SPEED * deltaTimeMs);
         } break;
         case LEFT: {
-            gameState->player.position.x -= (SPEED * deltaTimeMs);
+            newX -= (SPEED * deltaTimeMs);
         } break;
     }
 
-    //TODO: make a queue of inputs so we don't lose input directions if they press before a SQUARE_SIZE boundary
-    // and maybe think or another way to do this so it doesn't feel liek there is some input lag
-    if (((int)gameState->player.position.x) % SQUARE_SIZE == 0 && ((int)gameState->player.position.y) % SQUARE_SIZE == 0) {
-        gameState->player.direction = gameState->inputDirection;
+    //TODO: allow travel from end/start of screen to start/end of screen
+    if (newX < 0) {
+        newX = 0;
+    } else if (newX + SQUARE_SIZE > width) {
+        newX = width - SQUARE_SIZE;
     }
+
+    if (newY < 0) {
+        newY = 0;
+    } else if (newY + SQUARE_SIZE > height) {
+        newY = height - SQUARE_SIZE;
+    }
+
+    gameState->player.position.x = newX;
+    gameState->player.position.y = newY;
+
+    // TODO: this is a very bad way to do this because it only changes the direction if it happends to land right at the boundary
+    // of a square, which is not enforced in any way and makes it feel like input lag, I think a better way to do do this is to check if
+    // it crossed a square boundary with the last position update
+    //if (((int)gameState->player.position.x) % SQUARE_SIZE == 0 && ((int)gameState->player.position.y) % SQUARE_SIZE == 0) {
+        gameState->player.direction = gameState->inputDirection;
+    //}
 }
 
 void gameUpdateAndRender(WindowBuffer *windowBuffer, GameState *gameState, float deltaTimeMs) {
     drawGrid(windowBuffer);
 
-    updatePlayerPosition(gameState, deltaTimeMs);
+    updatePlayerPosition(gameState, windowBuffer->width, windowBuffer->height, deltaTimeMs);
 
     int playerX = gameState->player.position.x;
     int playerY = gameState->player.position.y;
 
-    //TODO: check window boundaries do this doesn't cause segfault at the end of window
     int pitch = windowBuffer->width * windowBuffer->bytesPerPixel;
     for (int y = playerY; y < playerY + SQUARE_SIZE; ++y) {
         char *row = windowBuffer->memory + (y * pitch);
